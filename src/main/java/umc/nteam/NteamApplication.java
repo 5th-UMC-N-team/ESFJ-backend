bb9a92a07bc35411ea71c46135f12cdf2c03371b
Kim Doo Hyeon <engus525@naver.com>
2024-01-02 14:20:41 +0900

    gogi: project init

---
+++ src/main/java/umc/nteam/NteamApplication.java
@@ -0,0 +1,13 @@
+package umc.nteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NteamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NteamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/umc/nteam/NteamApplicationTests.java
@@ -0,0 +1,13 @@
+package umc.nteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NteamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
