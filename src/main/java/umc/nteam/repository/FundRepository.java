c50b3b2dd9e43f054ad0464407e94ac9a2fea8fc
jina kong <kongjn226@gmail.com>
2024-01-03 03:49:19 +0900

    jina: [FEAT] 위시 모금현황 조회 Service 구현 #52

--- src/main/java/umc/nteam/repository/FundRepository.java
+++ src/main/java/umc/nteam/repository/FundRepository.java
@@ -1,6 +1,8 @@
 package umc.nteam.repository;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import umc.nteam.domain.Fund;
 import umc.nteam.domain.Wish;
@@ -8,4 +10,6 @@ import umc.nteam.domain.Wish;
 public interface FundRepository extends JpaRepository<Fund, Long> {
 
     List<Fund> findAllByWish(Wish wish);
+
+    List<Fund> findAllByWishId(Long wishId);
 }
