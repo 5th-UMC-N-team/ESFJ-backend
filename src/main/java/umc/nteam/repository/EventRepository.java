91c3577cd21b77e115aedea5474413bc82457912
JinhyeokBang <jinhyeokfang@gmail.com>
2024-01-03 01:25:07 +0900

    jinhyeok: [FIX] Repository 오타 수정, 누락된 Service 파일 추가 #39

--- src/main/java/umc/nteam/repository/EventRepository.java
+++ src/main/java/umc/nteam/repository/EventRepository.java
@@ -1,7 +1,7 @@
 package umc.nteam.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import umc.nteam.domain.Event;
 
-public interface EventRepository extends JpaRepository<EventRepository, Long> {
+public interface EventRepository extends JpaRepository<Event, Long> {
 }
